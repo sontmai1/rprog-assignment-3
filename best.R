0b8f01ab2e19ce77b666c92a2dd1ff4ee48f5e08
Mai, Son <son.mai@warnermedia.com>
2023-03-29 16:51:29 -0400

    Updated best.R

--- best.R
+++ best.R
@@ -1,23 +1,13 @@
 ## 2 Finding the best hospital in a state
 
 best <- function(state, outcome) {
-    # state <- "TX"
-    # outcome = "heart attack"
-    
+
     ## Read outcome data
     outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    #outcome_data[, 11] <- as.numeric(outcome_data[, 11])
-    
-    # valid_states <- unique(outcome_data$State)
-    
-    # select the columns you want to extract
-    #selected_cols <- c("Hospital.Name", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack")
-    
-    ## Check and remove rows that state and outcome are invalid
-    # valid_outcome <- na.omit(outcome_data)
-    valid_state_outcome <- outcome_data[outcome_data$State == state, ]
-    
+
     if (any(outcome_data$State %in% state)) {
+        
+        valid_state_outcome <- outcome_data[outcome_data$State == state, ]
         
         lowest_rate_rows <- NULL
 
@@ -45,12 +35,7 @@ best <- function(state, outcome) {
             lowest_rate <- min(valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)
             lowest_rate_rows <- valid_state_outcome[valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == lowest_rate, ]
         }
-        else
-        {
-            invalid_state <-paste0( "Error in best(", state , ",", outcome ,"): invalid outcome")
-            #invalid_state
-        }
-        
+
         ## Return hospital name in that state with lowest 30-day death
         ## rate
         
@@ -67,6 +52,4 @@ best <- function(state, outcome) {
     else{
         message(paste0( "Error in best(", state , ",", outcome ,"): invalid state"))
     }
-    
-    
 }
