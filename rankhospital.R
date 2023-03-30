38a8f6155ac5a1eb85115eb82775f045f256ceb3
Son Mai <son.thanh.mai@gmail.com>
2023-03-30 08:53:31 -0400

    Update rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,15 +1,9 @@
-
+## 3. Ranking hospitals by outcome in a state
+## Read outcome data
+## Check that state and outcome are valid
+## Return hospital name in that state with the given rank
+## 30-day death rate
 rankhospital <- function(state, outcome, num = "best") {
-    
-    # state = "TX"
-    # outcome = "heart failure"
-    # num = 4
-    
-    
-    ## Read outcome data
-    ## Check that state and outcome are valid
-    ## Return hospital name in that state with the given rank
-    ## 30-day death rate
     
     ## Read outcome data
     outcome_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -22,74 +16,62 @@ rankhospital <- function(state, outcome, num = "best") {
             valid_state_outcome[, 11] <- as.numeric(valid_state_outcome[, 11])
             valid_state_outcome <- na.omit(valid_state_outcome)
             
-            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)
+            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, 
+                                          Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack)
         }
         
         else if(outcome == "heart failure"){
-            #outcome_data[, 17] <- as.numeric(outcome_data[, 17])
+            
             valid_state_outcome[, 17] <- as.numeric(valid_state_outcome[, 17])
             valid_state_outcome <- na.omit(valid_state_outcome)
             
-            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)
+            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, 
+                                          Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure)
         }
         else if(outcome == "pneumonia"){
-            # outcome_data[, 23] <- as.numeric(outcome_data[, 23])
+            
             valid_state_outcome[, 23] <- as.numeric(valid_state_outcome[, 23])
             valid_state_outcome <- na.omit(valid_state_outcome)
 
-            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)            
+            rank_rate_state <- data.frame(Hospital.Name = valid_state_outcome$Hospital.Name, 
+                                          Rate = valid_state_outcome$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia)            
         }
         
-        ## Return hospital name in that state with lowest 30-day death
-        ## rate
+        ## Return hospital name in that state with lowest 30-day death rate
         
         if (is.null(rank_rate_state)) {
-            message(paste0( "Error in rankhospital(", state , ",", outcome ,",", num ,"): invalid outcome"))
+            stop("invalid outcome")
         } 
         else {
-            # sort by Hospital Name
-            sorted_result <- rank_rate_state[order(rank_rate_state$Rate),]
             
-
+            # sort by Rate, and Hospital.Name
+            sorted_result <- rank_rate_state[order(rank_rate_state$Rate, rank_rate_state$Hospital.Name),]
+            
             if (is.numeric(num)) {
                 
-                # print("my_num is a numeric value.")
                 rank <- as.numeric(num)
-                # print(sorted_result[rank,]$Hospital.Name)
                 result <- sorted_result[rank,]$Hospital.Name
                 result
                 
-                
             } else {
-                # print("my_num is not a numeric value: ")
                 
                 if(num == "best") {
-                    # print("rank == best ...")
-                    
-                    # sort by Hospital Name increasing
-                    
+                   
                     result <- sorted_result[1,]$Hospital.Name
-                    # print(result)
                     result
                 }
                 else if(num == "worst") {
-                    # print("rank == worst ...")
-                    
-                    # sort by Hospital Name decreasing
-                    # sorted_result <- lowest_rate_rows[order(decreasing = TRUE, lowest_rate_rows$Hospital.Name),]
-                    # sorted_result[1,]$Hospital.Name
                     result <- tail(sorted_result, n = 1)$Hospital.Name
-                    # print(result)
                     result
                 }
                 else {
-                    message(paste0( "Error in rankhospital(", state , ",", outcome ,",", num ,"): invalid num"))
+                    stop("invalid num")
                 }
             }
         }
     }
     else{
-        message(paste0( "Error in rankhospital(", state , ",", outcome ,",", num ,"): invalid state"))
+        stop("invalid state")
     }
     
 }
