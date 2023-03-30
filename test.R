97c9e56d97821836344e5e0039b13957d9bde701
Son Mai <son.mai@warnermedia.com>
2023-03-30 08:48:38 -0400

    Update test.R

--- test.R
+++ test.R
@@ -3,27 +3,25 @@
 source("best.R")
 
 best("TX", "heart attack")
-
 best("TX", "heart failure")
-
 best("MD", "heart attack")
-
 best("MD", "pneumonia")
-
 best("BB", "heart attack")
-
 best("NY", "hert attack")
-
 
 ############## TEST: rankhospital.R ######################
 
 source("rankhospital.R")
 
-rankhospital("TX", "heart attack")
-
 rankhospital("TX", "heart failure", 4)
 rankhospital("MD", "heart attack", "worst")
-rankhospital("MD", "heart attack")
 rankhospital("MN", "heart attack", 5000)
 rankhospital("GA", "heart attack")
 
+############## TEST: rankall.R ######################
+
+source("rankall.R")
+
+head(rankall("heart attack", 20), 10)
+tail(rankall("pneumonia", "worst"), 3)
+tail(rankall("heart failure"), 10)
